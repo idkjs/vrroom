ab3eda4f89c9f50b27d2fa90bbccf52d2a48e1ba
Alain Armand <idkjs@users.noreply.github.com>
2021-05-28 20:53:08 +0200

    update webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,29 @@
 const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const outputDir = path.join(__dirname, 'dist/');
+
+const isProd = process.env.NODE_ENV === 'production';
 
 module.exports = {
+  mode: isProd ? 'production' : 'development',
   entry: './examples/index.bs.js',
   output: {
-    path: path.join(__dirname, "build"),
-    filename: 'index.js',
+    path: outputDir,
+    filename: 'index.[fullhash].js',
+    publicPath: '/'
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Vroom Examples',
+      template: 'examples/index.html'
+    })
+  ],
+  devServer: {
+    compress: true,
+    contentBase: outputDir,
+    port: process.env.PORT || 8080,
+    historyApiFallback: true,
+    stats: 'minimal'
+  }
 };
